c5985ba2b6dbcea9fc9585c4d246964ed3e86962
Erick Clinton <erickleite2009321@gmail.com>
2024-08-13 09:30:30 -0300

    base structure

---
+++ src/main/java/com/erick/clinton/sqss3api/Sqss3apiApplication.java
@@ -0,0 +1,13 @@
+package com.erick.clinton.sqss3api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sqss3apiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sqss3apiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erick/clinton/sqss3api/Sqss3apiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erick.clinton.sqss3api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sqss3apiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
