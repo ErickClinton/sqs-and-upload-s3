4a5aede86161642617a4576636d6953b17946cf9
Erick Clinton <erickleite2009321@gmail.com>
2024-08-13 09:43:45 -0300

    config mongodb

--- src/main/java/com/erick/clinton/sqss3api/Sqss3apiApplication.java
+++ src/main/java/com/erick/clinton/sqss3api/Sqss3apiApplication.java
@@ -2,8 +2,10 @@ package com.erick.clinton.sqss3api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class Sqss3apiApplication {
 
 	public static void main(String[] args) {
